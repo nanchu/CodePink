a4b112915d0151559fb297eed65777991eb7af33
optimusChu <nchu@thoughtworks.com>
2012-08-13 13:10:05 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codepink::Application.config.secret_token = 'f74272e20d92b1c261c70d0bfd14ff36576a396f3badbacb21c9c626af6515fb93dbd9098af4830def683a5c0c5b3be77f1dbb543fddafc78976001067af5dde'

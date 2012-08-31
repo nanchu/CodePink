0b81c17d217ead361232236c0bda8f4dbcf0c71a
korykraft <kory@kraft>
2012-08-30 17:21:05 -0700

    added user signup/signout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120829224131) do
+ActiveRecord::Schema.define(:version => 20120830233626) do
 
   create_table "drone_attacks", :force => true do |t|
     t.integer  "incident_year"
@@ -78,8 +78,10 @@ ActiveRecord::Schema.define(:version => 20120829224131) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,11 +1,32 @@
 require 'spec_helper'
 
-describe "AuthenticationPages" do
-  describe "GET /authentication_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get authentication_pages_index_path
-      response.status.should be(200)
+describe "Authentication" do
+
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path}
+
+    describe "with invalid info" do
+      before { click_button "Sign in"}
+
+      it { should have_selector('h1', text: 'Sign in')}
+    end
+
+    describe "with valid info" do
+      let(:user) { FactoryGirl.create(:user)}
+      before do
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      it { should have_selector('h1', text: 'Locations')}
+
+      describe "follwed by signout" do
+        before { click_link "Sign out"}
+        it { should have_link('Sign in')}
+      end
     end
   end
 end
